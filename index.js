12977ffea25f2781db52d4a2b218f92eec5451a8
Yeohoon <yhoon.yoon@gmail.com>
2022-01-09 17:12:58 -0500

    commit tree 7d2d2358daa1c3065f5ca98c155d3c2f53c2556e

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.trunc(Math.random()*10) +1;
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = (1, 2, 3, 4,20);
+Math.max(max);
